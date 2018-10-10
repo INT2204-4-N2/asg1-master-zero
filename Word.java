2fefca744b8bc51b863392e57dad1cf9f5f386bf
truongphat17020948 <43178938+truongphat17020948@users.noreply.github.com>
2018-10-11 00:14:41 +0700

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,25 @@
+package masterzero;
+import java.util.Scanner;
+
+public class Word {
+    String getWord_target() {
+        return word_target;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    String word_target;
+
+    public String getWord_plain() {
+        return word_plain;
+    }
+
+    public void setWord_plain(String word_plain) {
+        this.word_plain = word_plain;
+    }
+
+    String word_plain;
+
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+package masterzero;
+class main{
+public static void main(String Abc[]) {
+    DictionaryComanline test= new DictionaryComanline();
+    test.dictionaryAdvanced();
+
+
+}}
