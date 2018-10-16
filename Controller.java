bdf4df4a393d2e4639fd0029d9d61996f9eaf264
truongphat17020948 <43178938+truongphat17020948@users.noreply.github.com>
2018-10-17 00:11:02 +0700

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -4,7 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
 public class Controller {
     @FXML
     private TextField text1= new TextField();
@@ -15,32 +19,33 @@ public class Controller {
     @FXML
      private  Button s2=new Button();
     @FXML
-    private ListView list2 = new ListView();
+    private  TextField text2 = new TextField();
+    @FXML
+    private TextField text3 = new TextField();
     @FXML
     private Button s3 = new Button();
     @FXML
-    private  String a = "";
+    private  String e = "";
+    @FXML Button s4 = new Button();
+    @FXML
+    private WebView w1 = new WebView();
+    @FXML
        public void search(ActionEvent event)
    {
        DictionaryComanline c= new DictionaryComanline();
        DictionaryManagement a=new DictionaryManagement();
        Dictionary b =  new Dictionary();
-       b.setDictionarys(a.insertFromfile());
-
-      // list1.getItems().setAll(a.dictionarylookup(b.getDictionarys(),text1.getText()));
-      // list1.getItems().setAll(c.dictionarySearch(b.getDictionarys(),text1.getText()));
-      // text1.clear();
-      // text1.appendText("house");
-      // list1.getItems().setAll( c.dictionarySearch(text1.getText(),b.getDictionarys()));
-       list2.getItems().setAll(a.dictionarylookup(b.getDictionarys(),text1.getText()));
+    a.insertFromFile(b);
+       WebEngine webEngine= w1.getEngine();
+       webEngine.loadContent(a.dictionarylookup(b.getDictionarys(),text1.getText()));
 
    }
-   public void Find (ActionEvent event)
+   public void Find (KeyEvent event)
    {
        DictionaryComanline c= new DictionaryComanline();
        DictionaryManagement a=new DictionaryManagement();
        Dictionary b =  new Dictionary();
-       b.setDictionarys(a.insertFromfile());
+   a.insertFromFile(b);
        list1.getItems().setAll(c.dictionarySearch(b.getDictionarys(),text1.getText()));
 
    }
@@ -48,11 +53,35 @@ public class Controller {
    public void click1 ()
    {
        text1.clear();
+       list1.getItems().setAll(" ");
    }
+
    public void Mouse(MouseEvent arg0)
    {
-      // a = output.getSelectionModel().getSelectedItem().toString();
-       //input.setText(a);
+       DictionaryComanline c= new DictionaryComanline();
+       DictionaryManagement a=new DictionaryManagement();
+       Dictionary b =  new Dictionary();
+      a.insertFromFile(b);
+       e = list1.getSelectionModel().getSelectedItem().toString();
+       text1.setText(e);
+       WebEngine webEngine= w1.getEngine();
+       webEngine.loadContent(a.dictionarylookup(b.getDictionarys(),text1.getText()));
+   }
+   public void delete(ActionEvent event)
+   {
+       DictionaryComanline c= new DictionaryComanline();
+       DictionaryManagement a=new DictionaryManagement();
+       Dictionary b =  new Dictionary();
+       a.insertFromFile(b);
+       a.dictionaryExportToFile(text2.getText(),b.getDictionarys());
+   }
+   public void add()
+   {
+       DictionaryComanline c= new DictionaryComanline();
+       DictionaryManagement a=new DictionaryManagement();
+       Dictionary b =  new Dictionary();
+       a.insertFromFile(b);
+       a.addfile(b.Dictionarys,text2.getText(),text3.getText());
    }
 
 }
