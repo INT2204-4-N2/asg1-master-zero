321ad0a0caac390b0fc792d191bcd3c311e1c2af
truongphat17020948 <43178938+truongphat17020948@users.noreply.github.com>
2018-10-17 00:13:19 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,15 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Tù Điển Phát Đẹp Trai");
+
+        Scene scene = new Scene(root, 600, 600);
+        scene.getStylesheets().add(getClass().getResource("Css.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
