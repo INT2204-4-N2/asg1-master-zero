dc5ad740d39ec0ceb9fb5b1090a93c4d25dfad3e
truongphat17020948 <43178938+truongphat17020948@users.noreply.github.com>
2018-10-17 00:12:44 +0700

    Update DictionaryManagement.java

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -1,8 +1,8 @@
-package masterzero;
-import java.io.FileNotFoundException;
+package sample;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.File;
+
 public class DictionaryManagement {
 
     public Word insertWord() {
@@ -39,39 +39,101 @@ public class DictionaryManagement {
         return a;
     }
 
-    public ArrayList<Word> insertFromfile() {
-        ArrayList filearray = new ArrayList();
-        File file = new File("C:/gibhub/filedictionary/abc.txt");
-        try (Scanner sc = new Scanner(file)) {
+        public void insertFromFile(Dictionary as) {
+        String path="C:/gibhub/filedictionary/E_V.txt";
+            String[] wordonline;
+            String textInALine;
+            BufferedReader br = null;
+            int i=0;
+            try {
+                br = new BufferedReader(new FileReader(path));
+                while ((textInALine = br.readLine()) != null) {
+                    int posSplit = textInALine.indexOf("<html>");
+                    if (posSplit > 0 && posSplit < textInALine.length()) {
+                        String target = textInALine.substring(0, posSplit);
+                        String explain = textInALine.substring(posSplit);
+                        Word word = new Word();
+                        word.word(target,explain);
+                        as.push(word);
 
-            for (int i = 0; i < 30000; i++) {
-                Word fileWord = new Word();
-                String str = new String();
-                str = sc.next();
-                fileWord.setWord_target(str);
-                str = sc.nextLine();
-                fileWord.setWord_plain(str);
-                filearray.add(i, fileWord);
+                    }
+                }
+
+
             }
-        } catch (Exception e) {
-        }
+            catch (Exception e) {}
 
-
-        return filearray;
     }
 
-    public void dictionarylookup(ArrayList<Word> a) {
+    public String dictionarylookup(ArrayList<Word> a,String text1) {
+
+        ArrayList<String> test=new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        String b;
-        System.out.print("seach targer : ");
-        b =sc.next() ;
         for (int i = 0; i < a.size(); i++) {
             Word swap = new Word();
             swap = a.get(i);
-            if (swap.word_target.equals(b)) {
-                System.out.println("planin:"+swap.getWord_plain());
+            if (swap.word_target.equals(text1)) {
+                text1=swap.word_plain;
+            }
+        }
+        return text1;
+    }
+    public void dictionaryExportToFile(String text1,ArrayList<Word> a) {
+        File file = new File("C:/gibhub/filedictionary/abc.txt");
+        for (int i = 0; i < a.size(); i++) {
+            Word swap = new Word();
+            swap = a.get(i);
+            if (swap.word_target.equals(text1)) {
+                a.remove(i);
+            }
+        }
+     //   insertfile(a,file);
+
+    }
+    public void insertfile(ArrayList<Word> arrayList1,File qq)
+    {
+        try{
+            PrintWriter pw = new PrintWriter(qq);
+            for (int i = 0 ; i<arrayList1.size();i++)
+            {
+                Word swap = new Word();
+                swap = arrayList1.get(i);
+                pw.print(swap.getWord_target());
+                pw.println(swap.getWord_plain());
+            }}
+        catch (Exception e){
+
+        }
+    }
+    public  void addfile(ArrayList<Word> add,String texttess,String texttess2) {
+        File file = new File("C:/gibhub/filedictionary/abc.txt");
+        int n = 0;
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        for (int i = 0; i < add.size(); i++) {
+            Word s=new Word();
+            s=add.get(i);
+            if (s.word_target.equals(texttess)) {
+                n++;
+            }
+        }
+        if (n == 0) {
+            try {
+                fw = new FileWriter(file.getAbsoluteFile(), true);
+                bw = new BufferedWriter(fw);
+                bw.write(texttess+" "+texttess2+"\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (bw != null)
+                        bw.close();
+                    if (fw != null)
+                        fw.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
 }
-
